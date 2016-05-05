ed1bd0c7e6b27f25fb92e0c6fd4065e4c22dd1ac
Lens Story Filmes <lensstoryfilmes@gmail.com>
2016-05-05 15:21:54 -0300

    wq

--- include/js/main.js
+++ include/js/main.js
@@ -77,11 +77,11 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''}    
+											{image : 'include/img/slider-images/image01.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image02.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image03.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image04.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''},
+											{image : 'include/img/slider-images/image04.jpg', title : '<div class="slide-content">Lens Story Filmes</div>', thumb : '', url : ''}    
 									],
 									
 		// Theme Options			   
@@ -194,7 +194,7 @@ BRUSHED.contactForm = function(){
 		
 		$.ajax({
 			type: "POST",
-			url: "_include/php/contact.php",
+			url: "include/php/contact.php",
 			data: fields,
 			dataType: 'json',
 			success: function(response) {
@@ -402,7 +402,7 @@ $(document).ready(function(){
 	Modernizr.load([
 	{
 		test: Modernizr.placeholder,
-		nope: '_include/js/placeholder.js', 
+		nope: 'include/js/placeholder.js', 
 		complete : function() {
 				if (!Modernizr.placeholder) {
 						Placeholders.init({
